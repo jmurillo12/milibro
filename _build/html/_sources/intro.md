5e64bf4aa0547d86994ecfbdcb2cfaf716ade96c
jmurillo12 <jcmurillo7@utpl.edu.ec>
2022-10-25 01:42:35 -0500

    Mi primer commit

---
+++ _build/html/_sources/intro.md
@@ -0,0 +1,14 @@
+# CENTRO DE ESTÉTICA Y SALÓN DE BELLEZA
+
+# DIAGNÓSTICO
+
+Para garantizar unos resultados óptimos en nuestro trabajo, recomendamos comenzar tus tratamientos con nuestro diagnóstico facial personalizado.
+Será nuestro punto de partida para conocer sus singularidades, ayudando a prevenir patologías futuras y mejorando las problemáticas actuales.
+Un asesoramiento realizado por profesionales que te asesorarán y atenderán para que saques el mayor rendimiento a tu piel con un plan completo de rutinas de belleza en casa y en cabina.
+
+![alt text](2.jpg)
+
+Pide ahora tu cita para un diagnóstico facial personalizado y deja tu piel en nuestras manos.
+
+```{tableofcontents}
+```
---
+++ _build/jupyter_execute/markdown-notebooks.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # RITUALES SPA
+# 
+# Ritual energizante inspirado en la litoterapia marina, para disfrutar del poder de las piedras y recargar el cuerpo de energía vital.
+# Comenzamos con un gommage a base de Arena Rosa Coralina y Cuarzo Blanco con funciones dinamizadoras que activan los flujos energéticos.
+# A continuación, entraríamos ya en el masaje. Una combinación de una crema de masaje, con un concentrado de “Alga Preciosa”
+# enriquecida en Oro y Gemas de Cristal de Roca. Un momento sensorial intenso que, gracias a la textura innovadora y al tacto de las piedras, 
+# deja la piel delicadamente satinada y favorece la eficacia del masaje.
+# 
+# ![](ritual-joyaux.jpg) 
+# 
+# ## RITUAL ÎLES PACIFIQUE
+# 
+# Ritual relajante con pindas de arena caliente inspirado en el movimiento de las olas, para una profunda relajación.
+# En la primera etapa tiene lugar el Gommage des illes. Este gommage con arena blanca de Bora Bora, sal marina y cáscara de coco purifica el cuerpo con toda la suavidad y deja la piel suave y aterciopelada. 
+# Perfumado y enriquecido con extractos de coco y vainilla, te traslada al paraíso.
+# 
+# ![](rituel-relaxant.jpg) 
+# 
+# ## RITUAL CLEOPATRA
+# 
+# Este ritual se basa en las rutinas de belleza de Cleopatra y te harán sentir como una reina. 
+# La bienvenida se realiza con unas toallitas calientes impregnadas en un Elixir para continuar con un peeling de sal marina y leche.
+# 
+# ![](ritual-cleopatra.jpg) 
+# 
+# ## RITUAL TALASOTERAPIA
+# 
+# Ritual de talasoterapia con algas mircroestalladas 100% puras y naturales, ricas en minerales, vitaminas y oligoelementos.
+# Comenzamos con una exfoliación marina que favorece la renovación celular y la absorción de las propiedades de las algas.
+# 
+# ![](ritual-talasoterapia.jpg)
